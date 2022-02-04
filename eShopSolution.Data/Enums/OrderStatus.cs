b92ee0cc1973bae6e62b76e3c3b28c1684206a2b
Vũ Hải <kaizy1227@gmail.com>
2022-02-04 19:07:59 +0700

    add EF and DbContext

--- eShopSolution.Data/Enums/OrderStatus.cs
+++ eShopSolution.Data/Enums/OrderStatus.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace eShopSolution.Data.Enums
 {
-    internal class OrderStatus
+    public enum OrderStatus
     {
+        InProgress,
+        Confirmed,
+        Shipping,
+        Success,
+        Canceled
     }
 }
--- eShopSolution.Data/Enums/TransactionStatus.cs
+++ eShopSolution.Data/Enums/TransactionStatus.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace eShopSolution.Data.Enums
 {
-    internal class TransactionStatus
+    public enum TransactionStatus
     {
+        Success,
+        Failed
     }
 }
